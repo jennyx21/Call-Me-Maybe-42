59da9a269ec9b28115ab7c44f28b91ab89065d2c
jennyx21 <jtruckse@student.42heilbronn.de>
2026-07-29 08:05:16 +0200

    wip: changing from complete llm json generation to just func name and parameter extraction

--- src/__main__.py
+++ src/__main__.py
@@ -1,7 +1,7 @@
 from llm_sdk import Small_LLM_Model
 from src.parse import JsonLoader, ValidatorError
 from src.funktiocall import FunctionCall
-# from src.generator import generator, allow_ids
+# from src.generator import generator
 from src.llm_prompt import llm_prompt
 from pathlib import Path
 import json
@@ -10,47 +10,32 @@ ROOT = Path(__file__).resolve().parent.parent
 
 prompts = str(ROOT / "data" / "input" / "function_calling_tests.json")
 definitions = str(ROOT / "data" / "input" / "functions_definition.json")
-# prompts = ("/goinfre/jtruckse/Call-Me-Maybe-42/data/"
-#            "input/function_calling_tests.json")
-# definitions = ("/goinfre/jtruckse/Call-Me-Maybe-42/data/"
-#                "input/functions_definition.json")
 
-
-# def main():
-#     try:
-#         prompt = JsonLoader(prompts).promt_validator()
-#         definition = JsonLoader(definitions).definitons_validator()
-#     except ValidatorError as e:
-#         print(e)
-#         return
-# for element in definition:
-#     print(element.name)
-# print(type(prompt))
-# print(type(definitions))
 
 def main():
     llm = Small_LLM_Model()
 
-    # id_token = {}
     try:
         prompt = JsonLoader(prompts).promt_validator()
         definition = JsonLoader(definitions).definitons_validator()
     except ValidatorError as e:
         print(e)
         return
-    # id_token = id_to_token(llm)
     results = []
     for p in prompt:
         instructions = llm_prompt(definition, p)
-        # output_raw = generator(instructions, p.prompt, allowed_ids, llm)
-        output_raw = {"name": "fn_something",
-                      "parameters": {"a": 2, "b": 3}}
+        # name, parameters = generator(instructions, p.prompt, llm)
+        name = "name"
+        parameter = {"a": 2, "b": 3}
+        output_raw = {"name": name,
+                      "parameters": parameter}
+        print(output_raw)
         funktion = FunctionCall(name=output_raw["name"],
                                 arguments=output_raw["parameters"])
         print(funktion)
         name = funktion.name
         arguments = funktion.arguments
-            
+
         results.append({"prompt": p.prompt, "name": name,
                         "parameters": arguments})
 
@@ -58,106 +43,6 @@ def main():
     output_path.parent.mkdir(parents=True, exist_ok=True)
     with open(output_path, "w", encoding="utf-8") as f:
         json.dump(results, f, indent=2)
-
-    # llm = Small_LLM_Model()
-    # prompt1 = "what is the sum of 1 + 1, the answer is "
-    # prompt2 = "how many letters has the word 'hello', the answer is "
-    # prompt3 = "what color has the sky, the answer is "
-    # prompt4 = "why do we have a nose, the answer is "
-
-    # id1 = llm.encode(prompt1)
-    # id2 = llm.encode(prompt2)
-    # id3 = llm.encode(prompt3)
-    # id4 = llm.encode(prompt4)
-
-    # print(id1)
-    # print(id2)
-    # print(id3)
-    # print(id4)
-
-    # token1 = id1[0].tolist()
-    # token2 = id2[0].tolist()
-    # token3 = id3[0].tolist()
-    # token4 = id4[0].tolist()
-
-    # i = 0
-    # logit_list1 = []
-    # while i < 10:
-    #     logit1 = llm.get_logits_from_input_ids(token1)
-    #     next_token = logit1.index(max(logit1))
-    #     logit_list1.append(next_token)
-    #     token1.append(next_token)
-    #     i += 1
-
-    # print(llm.decode(id1))
-    # print(logit_list1)
-    # print(llm.decode(logit_list1))
-
-    # i = 0
-    # logit_list2 = []
-    # while i < 10:
-    #     logit2 = llm.get_logits_from_input_ids(token2)
-    #     next_token = logit2.index(max(logit2))
-    #     logit_list2.append(next_token)
-    #     token2.append(next_token)
-    #     i += 1
-
-    # print(llm.decode(id2))
-    # print(logit_list2)
-    # print(llm.decode(logit_list2))
-
-    # i = 0
-    # logit_list3 = []
-    # while i < 10:
-    #     logit3 = llm.get_logits_from_input_ids(token3)
-    #     next_token = logit3.index(max(logit3))
-    #     logit_list3.append(next_token)
-    #     token3.append(next_token)
-    #     i += 1
-
-    # print(llm.decode(id3))
-    # print(logit_list3)
-    # print(llm.decode(logit_list3))
-
-    # i = 0
-    # logit_list4 = []
-    # while i < 10:
-    #     logit4 = llm.get_logits_from_input_ids(token4)
-    #     next_token = logit4.index(max(logit4))
-    #     logit_list4.append(next_token)
-    #     token4.append(next_token)
-    #     i += 1
-
-    # print(llm.decode(id4))
-    # print(logit_list4)
-    # print(llm.decode(logit_list4))
-
-    # promt = "welche funktion rechent 1 + 1? die antwort ist"
-    # ausgabe = []
-    # encodet = llm.encode(promt)
-    # logit = encodet[0].tolist()
-    # # logits = llm.get_logits_from_input_ids(encodet[0].tolist())
-    # i = 0
-    # while (i < 50):
-    #     logits = llm.get_logits_from_input_ids(logit)
-    #     next_token = logits.index(max(logits))
-    #     logit.append(next_token)
-    #     i += 1
-    # # print(logits)
-    # print(llm.get_path_to_tokenizer_file())
-    # with open(llm.get_path_to_tokenizer_file(), encoding="utf=8") as f:
-    #     print(f)
-    #     tokenizer = json.load(f)
-    #     print(tokenizer)
-    # print(llm.get_path_to_vocab_file())
-    # # with open(llm.get_path_to_vocab_file()) as f:
-    # #     print(f)
-    # #     vocab = json.load(f)
-    # #     print(vocab)
-    # print(llm.get_path_to_merges_file())
-    # print(llm.decode(logit))
-    # decodet = llm.decode(encodet)
-    # print(decodet)
 
 
 if __name__ == "__main__":
--- src/generator.py
+++ src/generator.py
@@ -1,27 +1,9 @@
 from src.parse import Definition
-import json
+from typing import Any
 from llm_sdk import Small_LLM_Model
 from enum import Enum
 
 
-def generator(text: str, prompt: str, a_ids: list[str], llm: Small_LLM_Model):
-    p = llm.encode(text)
-    token = p[0].tolist()
-    generated = []
+def generator(text: str, llm: Small_LLM_Model) -> tuple[str, dict[str, Any]]:
 
-    logits = []
-    for _ in range(20):
-        logit = llm.get_logits_from_input_ids(token)
-        for tokenid, value in enumerate(logit):
-            if tokenid not in a_ids:
-                logit[tokenid] = float("-inf")
 
-        next_token = logit.index(max(logit))
-        logits.append(next_token)
-        generated.append(next_token)
-        token.append(next_token)
-
-        print(llm.decode(next_token), end="")
-
-    print()
-    return llm.decode(generated)
